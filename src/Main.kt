52a24beef0ef0fa6996907d160029a41c6f0b5eb
C0NN3CT10N-L0ST <alexandreromba@hotmail.com>
2021-12-29 21:02:20 +0000

    Implementa o main loop de execuçao do programa

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,28 @@
 fun main() {
-    var menuOption: Int? = null
     do {
-        println(makeMenu())
-        menuOption = validateMenuOption()
+        // Get Menu Option
+        val menuOption = getMenuOption()
+        var success = -1
 
-        if (menuOption == null) println("Invalid response.\n")
-    } while (menuOption == null)
+        if (menuOption == 1) {
+            // Game Settings/Options
+            val playerName = validatePlayerName()
+            val useLegend = validateLegend()
+            val numLines = validateLines()
+            val numColumns = validateColumns()
+            val numMines = validateMines(numLines, numColumns)
 
-    if (menuOption == 1) {
-        // Game Settings/Options
-        val playerName = validatePlayerName()
-        val useLegend = validateLegend()
-        val numLines = validateLines()
-        val numColumns = validateColumns()
-        val numMines = validateMines(numLines, numColumns)
+            // Creates matrix terrain with the given settings and prints initial board
+            val matrixTerrain = createMatrixTerrain(numLines, numColumns, numMines)
+            val terrain = makeTerrain(matrixTerrain, useLegend)
+            println(terrain)
 
-        val matrixTerrain = createMatrixTerrain(numLines, numColumns, numMines)
-        val terrain = makeTerrain(matrixTerrain, useLegend)
-        println(terrain)
-    }
+            success = gameLoop(matrixTerrain, numLines, numColumns)
+            if (success == 0) {
+                println("You win the game!")
+            } else if (success == 1) {
+                println("You lost the game!")
+            }
+        }
+    } while (success != 2)
 }
